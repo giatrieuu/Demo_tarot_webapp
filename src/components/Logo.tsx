beae21ae7568e2214dcf71ff1030ef84fc09ee9e
giatrieuu <trieulgse160392@fpt.edu.vn>
2025-03-24 16:33:01 +0700

    update

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from "../../public/assets/logo.png"
+import logo from "/assets/logo.png"
 const Logo: React.FC = () => {
   return (
     <Link to="/" className="flex items-center space-x-2">
